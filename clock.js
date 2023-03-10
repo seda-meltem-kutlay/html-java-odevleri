8c677035700fb09ac04c9c18c01a24c6ea3e7d96
sedameltemkutlay <111974816+seda-meltem-kutlay@users.noreply.github.com>
2023-03-10 18:34:34 +0300

    saat odevi

---
+++ clock.js
@@ -0,0 +1,19 @@
+let userName = prompt("Kullanıcı Adınızı Giriniz:")
+
+function showTime(){
+
+
+    let myName =document.querySelector("#myName")
+    
+    myName.innerHTML = userName 
+    
+    let myClock = document.querySelector("#myClock")
+    var date =new Date()
+    
+    var gunler = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
+    
+    myClock.innerHTML = `
+    ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${gunler[date.getDay()]} 
+    `
+    }
+    setInterval(showTime,1000)
